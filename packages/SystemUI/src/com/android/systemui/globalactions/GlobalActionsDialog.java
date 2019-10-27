90224d0210a842b24a59900e7e057a08c883ff9e
ezio84 <brabus84@gmail.com>
2019-10-27 09:14:35 +0100

    Unlink screenrecord toggle from featureflags and enable it
    now that we got it working, there's no reason to not enable it
    
    Change-Id: Ia15108afbbbc75cf2548a6be53b45cd3474ed379

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -57,7 +57,6 @@ import android.telephony.ServiceState;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.ArraySet;
-import android.util.FeatureFlagUtils;
 import android.util.Log;
 import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
@@ -633,11 +632,7 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
 
         @Override
         public boolean onLongPress() {
-            if (FeatureFlagUtils.isEnabled(mContext, FeatureFlagUtils.SCREENRECORD_LONG_PRESS)) {
-                mScreenRecordHelper.launchRecordPrompt();
-            } else {
-                onPress();
-            }
+            mScreenRecordHelper.launchRecordPrompt();
             return true;
         }
     }
